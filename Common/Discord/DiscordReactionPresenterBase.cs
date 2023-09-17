102b219878515d754aeda0f4205472c0c0970375
Masaki Inoue <45062199+2RiniaR@users.noreply.github.com>
2023-09-18 04:48:25 +0900

    メッセージの実装 (#3)

--- Common/Discord/DiscordReactionPresenterBase.cs
+++ Common/Discord/DiscordReactionPresenterBase.cs
@@ -3,7 +3,7 @@ using Discord.WebSocket;
 
 namespace RineaR.Spring.Common;
 
-public abstract class DiscordReactionPresenterBase
+public abstract class DiscordReactionPresenterBase : DiscordPresenterBase
 {
     public SocketReaction Reaction { get; set; } = null!;
     public IUserMessage Message { get; set; } = null!;
--- Events/LoginPresenter.cs
+++ Events/LoginPresenter.cs
@@ -8,6 +8,9 @@ public class LoginPresenter : DiscordMessagePresenterBase
     public override async Task RunAsync()
     {
         var login = await UserServices.As(Message.Author.Id).LoginAsync();
-        if (login != null) await Message.ReplyAsync($"{Message.Author.Username}、今日も生きててえらい！ ✨{login.Score}");
+        if (login == null) return;
+
+        var text = $"{UserNameText(Message.Author)}、今日も生きててえらい！ {ScoreDiffText(login.Score)}";
+        await Message.ReplyAsync(text);
     }
 }
