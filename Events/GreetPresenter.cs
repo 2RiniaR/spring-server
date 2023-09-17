bcd9475656ce2d2aef4ac6be092952d3b38f01fa
watano1168 <watano1168@gmail.com>
2023-09-18 04:05:24 +0900

    Presenterを分離

--- Events/GreetPresenter.cs
+++ Events/GreetPresenter.cs
@@ -7,9 +7,6 @@ public class GreetPresenter : DiscordMessagePresenterBase
 {
     public override async Task RunAsync()
     {
-        var login = await UserServices.As(Message.Author.Id).LoginAsync();
-        if (login != null) await Message.ReplyAsync($"{Message.Author.Username}、今日も生きててえらい！ ✨{login.Score}");
-
         var wakeUp = await UserServices.As(Message.Author.Id).WakeUpAsync();
         if (wakeUp != null) await Message.ReplyAsync($"{Message.Author.Username}、おはよう！ ✨{wakeUp.Score}");
     }
