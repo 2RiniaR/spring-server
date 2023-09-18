ef24317b5a3e1948a3bbbbb6c73626d85d3cb29a
watano1168 <watano1168@gmail.com>
2023-09-18 09:10:43 +0900

    無理な共通化はしない

---
+++ Events/CancelComfortPresenter.cs
@@ -0,0 +1,12 @@
+﻿using RineaR.Spring.Common;
+
+namespace RineaR.Spring.Events;
+
+public class CancelComfortPresenter : DiscordReactionPresenterBase
+{
+    public override async Task RunAsync()
+    {
+        await UserServices.As(Reaction.UserId)
+            .CancelComfortAsync(AuthorUser.Id, Reaction.MessageId, Reaction.Emote.GetHashCode());
+    }
+}
---
+++ Events/ComfortPresenter.cs
@@ -0,0 +1,14 @@
+﻿using RineaR.Spring.Common;
+
+namespace RineaR.Spring.Events;
+
+public class ComfortPresenter : DiscordReactionPresenterBase
+{
+    public override async Task RunAsync()
+    {
+        var comfort = await UserServices.As(Reaction.UserId)
+            .ComfortAsync(AuthorUser.Id, Reaction.MessageId, Reaction.Emote.GetHashCode());
+
+        // ToDo: 一定数慰められたら何かしたい
+    }
+}
